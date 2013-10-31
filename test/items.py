c7ac5d84e021d4302119b87920119cd161e0786a
luofeilong <360777830@qq.com>
2013-10-31 19:40:24 +0800

    爬虫例子
    一个使用scrapy架构写的一个爬虫

---
+++ test/items.py
@@ -0,0 +1,19 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+from scrapy.item import Item, Field
+
+class TestItem(Item):
+    # define the fields for your item here like:
+    url = Field()
+    name = Field()
+    price = Field()
+    memprice = Field()
+    press = Field()
+    publication = Field()
+    author = Field()
+    desc = Field()
+    belong = Field()
+    pass
---
+++ test/settings.py
@@ -0,0 +1,17 @@
+# Scrapy settings for test project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'test'
+
+SPIDER_MODULES = ['test.spiders']
+NEWSPIDER_MODULE = 'test.spiders'
+
+ITEM_PIPELINES = ['test.pipelines.TestPipeline']
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+USER_AGENT = 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.66 Safari/537.36'
